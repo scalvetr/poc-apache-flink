d3820629a62bd73f5f42c239f39955b290d19a6b
Sergi Calvet <sergi.calvet@gmail.com>
2024-01-12 23:55:24 +0100

    sample scenario: data gen takes care of customer topic creation

--- sample-scenario/datagen/src/main/java/com/github/scalvetr/poc/flink/datagen/config/AppConfiguration.java
+++ sample-scenario/datagen/src/main/java/com/github/scalvetr/poc/flink/datagen/config/AppConfiguration.java
@@ -1,10 +1,21 @@
 package com.github.scalvetr.poc.flink.datagen.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 
 @Configuration
 @EnableConfigurationProperties(value = AppKafkaConfigurationProperties.class)
 public class AppConfiguration {
+
+    @Bean
+    public NewTopic topicExample(AppKafkaConfigurationProperties properties) {
+        return TopicBuilder.name(properties.topic())
+                .partitions(properties.partitions())
+                .replicas(properties.replicas())
+                .build();
+    }
 }
--- sample-scenario/datagen/src/main/java/com/github/scalvetr/poc/flink/datagen/config/AppKafkaConfigurationProperties.java
+++ sample-scenario/datagen/src/main/java/com/github/scalvetr/poc/flink/datagen/config/AppKafkaConfigurationProperties.java
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
 @ConfigurationProperties(prefix = "app.kafka")
-public record AppKafkaConfigurationProperties(String topic) {
+public record AppKafkaConfigurationProperties(String topic, int partitions, int replicas) {
 }
