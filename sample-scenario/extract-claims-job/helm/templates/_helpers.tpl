2236a4672dadef843ef917561aa4c79ee5c3c729
Sergi Calvet <sergi.calvet@gmail.com>
2024-01-15 00:04:46 +0100

    sample scenario: extract claims job

--- sample-scenario/extract-claims-job/helm/templates/_helpers.tpl
+++ sample-scenario/extract-claims-job/helm/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "cdc.name" -}}
+{{- define "extract-claims-job.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "cdc.fullname" -}}
+{{- define "extract-claims-job.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "cdc.chart" -}}
+{{- define "extract-claims-job.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "cdc.labels" -}}
-helm.sh/chart: {{ include "cdc.chart" . }}
-{{ include "cdc.selectorLabels" . }}
+{{- define "extract-claims-job.labels" -}}
+helm.sh/chart: {{ include "extract-claims-job.chart" . }}
+{{ include "extract-claims-job.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,17 +45,17 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "cdc.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "cdc.name" . }}
+{{- define "extract-claims-job.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "extract-claims-job.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
 
 {{/*
 Create the name of the service account to use
 */}}
-{{- define "cdc.serviceAccountName" -}}
+{{- define "extract-claims-job.serviceAccountName" -}}
 {{- if .Values.serviceAccount.create }}
-{{- default (include "cdc.fullname" .) .Values.serviceAccount.name }}
+{{- default (include "extract-claims-job.fullname" .) .Values.serviceAccount.name }}
 {{- else }}
 {{- default "default" .Values.serviceAccount.name }}
 {{- end }}
--- sample-scenario/extract-claims-job/src/main/java/com/github/scalvetr/ExtractClaimsJob.java
+++ sample-scenario/extract-claims-job/src/main/java/com/github/scalvetr/ExtractClaimsJob.java
@@ -17,13 +17,19 @@
 
 package com.github.scalvetr;
 
+import com.github.scalvetr.poc.flink.claims.model.Claim;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.mongodb.source.MongoSource;
 import org.apache.flink.connector.mongodb.source.enumerator.splitter.PartitionStrategy;
 import org.apache.flink.connector.mongodb.source.reader.deserializer.MongoDeserializationSchema;
+import org.apache.flink.formats.avro.AvroSerializationSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.bson.BsonDocument;
 import org.slf4j.Logger;
@@ -36,10 +42,8 @@ import java.util.Properties;
 /**
  * Main class for executing SQL scripts.
  */
-public class CDCJob {
-
-    private static final Logger LOG = LoggerFactory.getLogger(CDCJob.class);
-
+public class ExtractClaimsJob {
+    private static final Logger LOG = LoggerFactory.getLogger(ExtractClaimsJob.class);
 
     public static void main(String[] args) throws Exception {
         LOG.info("CDCJob");
@@ -58,7 +62,7 @@ public class CDCJob {
         String host = jobProperties.getProperty("mongodb.host");
         String port = jobProperties.getProperty("mongodb.port");
 
-        MongoSource<String> source = MongoSource.<String>builder()
+        MongoSource<Claim> source = MongoSource.<Claim>builder()
                 .setUri(String.format("mongodb://%s:%s@%s:%s", username, password, host, port))
                 .setDatabase(database)
                 .setCollection(collection)
@@ -68,26 +72,37 @@ public class CDCJob {
                 .setPartitionStrategy(PartitionStrategy.SAMPLE)
                 .setPartitionSize(MemorySize.ofMebiBytes(64))
                 .setSamplesPerPartition(10)
-                .setDeserializationSchema(new MongoDeserializationSchema<String>() {
+
+                .setDeserializationSchema(new MongoDeserializationSchema<Claim>() {
                     @Override
-                    public String deserialize(BsonDocument document) {
-                        return document.toJson();
+                    public Claim deserialize(BsonDocument document) {
+                        return Claim.newBuilder()
+                                .setClaimId(document.get("claimId").toString())
+                                .setCustomerId(document.get("customer_id").toString())
+                                .setPolicyId(document.get("policyId").toString())
+                                .setCreationTimestamp(document.get("creationTimestamp").asDouble().longValue()).build();
                     }
 
                     @Override
-                    public TypeInformation<String> getProducedType() {
-                        return BasicTypeInfo.STRING_TYPE_INFO;
+                    public TypeInformation<Claim> getProducedType() {
+                        return BasicTypeInfo.getInfoFor(Claim.class);
                     }
                 })
                 .build();
+        final KafkaSink<Tuple2<String, Claim>> sink = kafkaSinkUtils.buildKafkaSink(String.class, Claim.class,
+                new SimpleStringSchema(),
+                AvroSerializationSchema.forSpecific(Claim.class), outputTopic);
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "MongoDB-Source")
-                .setParallelism(2)
-                .print()
-                .setParallelism(1);
 
+        DataStream<Tuple2<String, Claim>> claims = env.fromSource(source, WatermarkStrategy.noWatermarks(), "MongoDB-Source")
+                .map(claim -> new Tuple2(claim.getClaimId(), claim));
+
+        claims.sinkTo(sink);
+
+
+        //env.enableCheckpointing(5000);
     }
 
     private static Properties loadProperties(String[] args, int i, String defaultValue) throws Exception {
@@ -95,7 +110,7 @@ public class CDCJob {
         if (args.length >= i + 1) {
             props.load(new FileInputStream(new File(args[i])));
         } else {
-            props.load(CDCJob.class.getClassLoader().getResourceAsStream(defaultValue));
+            props.load(ExtractClaimsJob.class.getClassLoader().getResourceAsStream(defaultValue));
         }
         return props;
     }
