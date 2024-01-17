ee4a5cb2210601156fa2747b561c697d86da3311
Sergi Calvet <sergi.calvet@gmail.com>
2024-01-18 00:07:50 +0100

    sample scenario: Claims helm configuration

--- sample-scenario/claims-cdc-job/src/main/java/com/github/scalvetr/ExtractClaimsJob.java
+++ sample-scenario/claims-cdc-job/src/main/java/com/github/scalvetr/ExtractClaimsJob.java
@@ -46,7 +46,7 @@ public class ExtractClaimsJob {
     private static final Logger LOG = LoggerFactory.getLogger(ExtractClaimsJob.class);
 
     public static void main(String[] args) throws Exception {
-        LOG.info("CDCJob");
+        LOG.info("ExtractClaimsJob");
         Properties jobProperties = loadProperties(args, 0, "job.properties");
         Properties kafkaSinkProperties = loadProperties(args, 1, "kafka-sink.properties");
 
@@ -55,6 +55,7 @@ public class ExtractClaimsJob {
         String outputTopic = jobProperties.getProperty("output.topic");
 
         String database = jobProperties.getProperty("mongodb.database");
+        String authDatabase = jobProperties.getProperty("mongodb.authDatabase");
         String collection = jobProperties.getProperty("mongodb.collection");
         String username = jobProperties.getProperty("mongodb.username");
         String password = jobProperties.getProperty("mongodb.password");
@@ -62,7 +63,7 @@ public class ExtractClaimsJob {
         String port = jobProperties.getProperty("mongodb.port");
 
         MongoSource<Claim> source = MongoSource.<Claim>builder()
-                .setUri(String.format("mongodb://%s:%s@%s:%s", username, password, host, port))
+                .setUri(String.format("mongodb://%s:%s@%s:%s?authSource=%s", username, password, host, port, authDatabase))
                 .setDatabase(database)
                 .setCollection(collection)
                 .setFetchSize(2048)
